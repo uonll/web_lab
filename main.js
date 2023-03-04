a87a240e08d8ba495f73fd1964b9234cdc748c0d
uonll <sofya.smirnova.2003@gmail.com>
2023-03-04 11:43:14 +0300

    lab 2

--- main.js
+++ main.js
@@ -1,119 +1,65 @@
 //задание 1
-let ums=20, ukmh=36;
-console.log(`${ukmh} м/c км/ч соответствует ${ukmh/3.6} м/c`);
-console.log(`${ums} м/c соответствует ${ums*3.6} км/ч`);
+function convertSpeed(v, dir) {
+    if (dir=='toMS') {
+        return(`${v} м/c км/ч соответствует ${v/3.6} м/c`); 
+    }
+    if (dir=='toKMH') {
+        return(`${v} м/c соответствует ${v*3.6} км/ч`);
+    }
+}
+
+console.log(convertSpeed(36, 'toMS'));
+console.log(convertSpeed(10, 'toKMH'));
 
 //задание 2
 console.log("");
-let a=10, b=8, c=6;
-if ((a+b>c) && (a+c>b) && (b+c>a))
-    {
-        let p=(a+b+c)/2;
-        console.log("треугольник существует");
-        console.log(` периметр = ${a+b+c}`);
-        console.log(` площадь = ${Math.sqrt(p*(p-a)*(p-b)*(p-c))}`);
-        console.log(` соотношение = ${(a+b+c)/(Math.sqrt(p*(p-a)*(p-b)*(p-c)))}`);
-
-    }    
-else {console.log("треугольник не существует");}   
-
+function absValue(n) {
+    if (n>=0) {
+        return n;
+    }
+    else {
+        return -n;
+    }
+}
+console.log(absValue(-2));
+console.log(absValue(100.1));
+console.log(absValue(0));
 
 //задание 3
 console.log("");
-let n = prompt('Введите натуральное число:');
-if (isNaN(n))
-    {
-        console.log("Вы не ввели число!");
-    }
-else
-    {
-        for (let i=0; i<=n; i++)
-            {
-                if (i%5==0 && i!=0) {console.log(`${i} fizz buzz`);}
-                else
-                    if (i%2==0) {console.log(`${i} buzz`);}
-                    else {console.log(`${i} fizz`);}
 
-            }
-    }
+let student = {
+    group: 235,
+    last_name: "Смирнова",
+    first_name: "Соня",
+    };
+props="";
+for (let prop in student) {
+        props+=prop+"; ";
+      }    
+console.log("Список свойств: ", props);
+console.log("Список свойств: ", Object.keys(student));
+console.log(`Студент ${student.last_name} ${student.first_name} учится в ${student.group} группе`);
 
 
 //задание 4
 console.log("");
-let star = "*";
-let grid = "#";
-let line = "";
-let numGrid = 2;
-let numStar = 1;
-let i=0;
-
-for(i = 0; i <= 11; i++)
-{
-    if(i % 2 != 0)
-        {           
-            line += grid.repeat(numGrid);
-            numGrid += 2; 
-            line += "\n"; 
-        }
-    else
-        {
-            line += star.repeat(numStar);
-            numStar += 2;
-            line += "\n"
-        }
-}
-line += "||";
-console.log(line);
+function randomNumber(min,max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+console.log(randomNumber(-10,10));
+console.log(randomNumber(0,10));
 
 //задание 5
 console.log("");
-let num=11;
-let z=NaN;
-do
-    {
-        z = prompt('Введите целое число:');
-        if (!isNaN(z))
-            {
-                if (z==num) {console.log("Вы угадали!");}
-                else
-                if (z>num) {console.log(`Число ${z} больше загаданного`);}
-                    else {console.log(`Число ${z} меньше загаданного`);}
-            }
-        else {console.log("Вы не ввели число!");}    
+function sampleArray (arr, k) {
+    let randomArr = [];
+    for (let i=0; i<k; i++) {
+        randomArr[i]=arr[randomNumber(0,(arr.length-1))];
     }
-while (z!=num);
-
-//задание 6
-console.log("");
-n=prompt('Введите целое ненулевое n');
-x=prompt('Введите целое ненулевое x:');
-y=prompt('Введите целое ненулевое y:');
-console.log(`n=${n}, x=${x}, y=${y} => ${n%x===0 && n%y===0}`);
-
-//задание 7
-console.log("");
-let m=prompt('Введите номер месяца:');
-switch (m) {
-    case '1':
-    case '2':
-    case '3':        
-        console.log(`месяц ${m} - 1 квартал`);
-        break;
-    case '4':
-    case '5':
-    case '6':        
-        console.log(`месяц ${m} - 2 квартал`);
-        break;    
-    case '7':
-    case '8':
-    case '9':        
-        console.log(`месяц ${m} - 3 квартал`);
-        break;
-    case '10':
-    case '11':
-    case '12':        
-        console.log(`месяц ${m} - 4 квартал`);
-        break;
-    default:
-        console.log("Такого месяца не существует!");
+    return randomArr;
 }
+console.log(sampleArray([1,2,3,4], 10));
+console.log(sampleArray([9,8,5,7], 2));
